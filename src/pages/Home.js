3d6b2de4e3b7d5fedce235f673a625f04ef03df0
Someswar Gorai <somgorai726@gmail.com>
2024-08-07 23:01:08 +0530

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import Products from '../components/Home/Products.jsx';
 import  {Clients}  from '../components/Home/Client.jsx';
 import Footer from '../components/Home/Footer';
 import Testimonials from "../components/Home/Testonomials.jsx"
+import { Helmet } from "react-helmet";
 
 export const Home = () => {
   
@@ -12,6 +13,9 @@ export const Home = () => {
 
   return (
     <div className="App overflow-x-hidden">
+      <Helmet>
+        <title>Smartone:Home</title>
+      </Helmet>
       <Header />
       <main className="animate-fade">
         <Hero />
